cd939cf6b3ac2670550033fc8cd5363409d840fb
rahilazhar12 <158289635+rahilazhar12@users.noreply.github.com>
2024-05-12 16:15:54 +0500

    Update Envroutes.jsx

--- client/src/Components/Envroutes.jsx
+++ client/src/Components/Envroutes.jsx
@@ -1,3 +1,3 @@
 // export const urlapi = "https://sportsdom-ffd56b130bf8.herokuapp.com"
-export const urlapi = "http://localhost:8000"
-// export const urlapi = "https://api.sportsdom.online"
+// export const urlapi = "http://localhost:8000"
+export const urlapi = "https://api.sportsdom.online"
